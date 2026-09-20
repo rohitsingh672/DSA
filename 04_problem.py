dbae7ad623fed8a492e306b540cdee53a7735dfa
Rohit <rohit0593singh@gmail.com>
2026-09-20 13:04:17 +0530

    Fix formatting of median calculation and add clarification on division behavior in Python

--- 04_problem.py
+++ 04_problem.py
@@ -6,7 +6,11 @@ nums2 = [3, 4]
 merge = sorted(nums1 + nums2) # [1, 2, 3, 4]
 n = len(merge) # 4
 if n % 2 == 0:
-    median = (merge[n//2 - 1] + merge[n//2]) / 2 # (merge[1] + merge[2]) / 2 = (2 + 3) / 2 = 2.5
+    median = ((merge[n//2 - 1] + merge[n//2]) / 2) # (merge[1] + merge[2]) / 2 = (2 + 3) / 2 = 2.5
 else:
     median = merge[n//2] # Not applicable in this case
 print(median) # median = 2.5
+
+# In python 2 when we perform division between two integer value then it provide me the integer value
+# Example: 5/2 = 2 in python 2
+# But in python 3 5/2 = 2.5 in the new version of python it changes the behaviour of /
