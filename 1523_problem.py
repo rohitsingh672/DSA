280b8f09399113ddad790f1860936dc66185aa17
rohitsingh672 <rohit9928720singh@gmail.com>
2026-09-20 18:01:25 +0530

    Add clarification comment on odd number count calculation

--- 1523_problem.py
+++ 1523_problem.py
@@ -1,5 +1,5 @@
 # Leetcode Problem 1523 : Count Odd Numbers in an Interval Range
 low = 3
 high = 9
-count = ((high + 1)//2 - low//2)
+count = ((high + 1)//2 - low//2) # Count odd numbers between given range
 print(count)
